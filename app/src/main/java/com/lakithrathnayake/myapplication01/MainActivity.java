3bd355adf4285ae6d735a188facda38e12e17f46
Lakith Rathnayake <lakithpathmila@gmail.com>
2025-01-24 09:23:40 +0530

    Add content provider example

--- app/src/main/java/com/lakithrathnayake/myapplication01/MainActivity.java
+++ app/src/main/java/com/lakithrathnayake/myapplication01/MainActivity.java
@@ -1,10 +1,15 @@
 package com.lakithrathnayake.myapplication01;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,52 +35,81 @@ public class MainActivity extends AppCompatActivity {
         Log.d(msg, "The onCreate() event");
     }
 
+
     /** Called when the activity is about to become visible. */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(msg, "The onStart() event");
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.d(msg, "The onStart() event");
+//    }
+//
+//    /** Called when the activity has become visible. */
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.d(msg, "The onResume() event");
+//    }
+//
+//    /** Called when another activity is taking focus. */
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.d(msg, "The onPause() event");
+//    }
+//
+//    /** Called when the activity is no longer visible. */
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.d(msg, "The onStop() event");
+//    }
+//
+//    /** Called just before the activity is destroyed. */
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        Log.d(msg, "The onDestroy() event");
+//    }
+//
+//    public void startService(View view) {
+//        startService(new Intent(getBaseContext(), MyService.class));
+//    }
+//
+//    public void stopService(View view) {
+//        stopService(new Intent(getBaseContext(), MyService.class));
+//    }
+//
+//    public void broadcastIntent(View view) {
+//        Intent intent = new Intent();
+//        intent.setAction("com.lakithrathnayake.CUSTOM_INTENT");
+//        sendBroadcast(intent);
+//    }
+
+    public void onClickAddName(View view) {
+        ContentValues values = new ContentValues();
+        values.put(StudentProvider.NAME, ((EditText)findViewById(R.id.editText2)).getText().toString());
+
+        values.put(StudentProvider.GRADE, ((EditText)findViewById(R.id.editText3)).getText().toString());
+
+        Uri uri = getContentResolver().insert(StudentProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
     }
 
-    /** Called when the activity has become visible. */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(msg, "The onResume() event");
-    }
 
-    /** Called when another activity is taking focus. */
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(msg, "The onPause() event");
-    }
+    public void onClickRetrieveStudents(View view) {
+        String URL = "content://com.lakithrathnayake.myapplication01.StudentProvider";
 
-    /** Called when the activity is no longer visible. */
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(msg, "The onStop() event");
-    }
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
 
-    /** Called just before the activity is destroyed. */
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(msg, "The onDestroy() event");
-    }
-
-    public void startService(View view) {
-        startService(new Intent(getBaseContext(), MyService.class));
-    }
-
-    public void stopService(View view) {
-        stopService(new Intent(getBaseContext(), MyService.class));
-    }
-
-    public void broadcastIntent(View view) {
-        Intent intent = new Intent();
-        intent.setAction("com.lakithrathnayake.CUSTOM_INTENT");
-        sendBroadcast(intent);
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
     }
 }
